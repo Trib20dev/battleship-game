3a868bb8aa5c189f2ba14c5615bbd072de82c675
Trib20dev <Triburondev@gmail.com>
2025-12-15 23:09:26 +0100

    Created a player class to store the boats

---
+++ src/main_container/Player.java
@@ -0,0 +1,22 @@
+package main_container;
+
+import java.util.ArrayList;
+
+public class Player {
+	ArrayList<Boat> boats;
+	int currentBoats = 0;
+	/* 
+	 * Carrier 5 coordinates
+	 * Battleship 4 coordinates
+	 * Cruiser 3 coordinates
+	 * Submarine 3 coordinates
+	 * Destroyer 2 coordinates
+	 */
+	public Player() {
+	}
+	
+	
+	
+	
+	
+}
