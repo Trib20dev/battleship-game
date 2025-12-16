5cca693b8a20e20fc301d847a37040f52fc65c14
Trib20dev <triburondev@gmail.com>
2025-12-16 08:20:46 +0100

    Added maxCoordinates and defined coordinates

--- src/main_container/Boat.java
+++ src/main_container/Boat.java
@@ -6,6 +6,12 @@ public class Boat {
 	private ArrayList<Coordinates> coordinates;
 	//(state) ? hit : not hit 
 	private ArrayList<Boolean> estado;
+	private int currentCoordinatesSet = 0;
+	private int maxCoordinates;
+	
+	public Boat(int maxCoordinates){
+	  this.maxCoordinates = maxCoordinates;
+	}
 	
 	public void addCoordiante(int row, int column) {
 		coordinates.add(new Coordinates(row, column));
