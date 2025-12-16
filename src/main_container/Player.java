2d9f3365df45b10a6d29bd6958d5018a8ce687a9
Trib20dev <triburondev@gmail.com>
2025-12-16 10:19:58 +0100

    Added boat names properties and a setBoatPosition initial structure

--- src/main_container/Player.java
+++ src/main_container/Player.java
@@ -4,7 +4,19 @@ import java.util.ArrayList;
 
 public class Player {
 	ArrayList<Boat> boats;
+	ArrayList<String> boatsNames;
+	//Adding the boats names
+	{
+	boatsNames.add("Carrier");
+	boatsNames.add("Battleship");
+	boatsNames.add("Cruiser");
+	boatsNames.add("Submarine");
+	boatsNames.add("Destroyer");
+	}
+	
+	int shots[][] = new int[10][10];
 	int currentBoats = 0;
+	
 	/* 
 	 * Carrier 5 coordinates
 	 * Battleship 4 coordinates
@@ -13,10 +25,15 @@ public class Player {
 	 * Destroyer 2 coordinates
 	 */
 	public Player() {
+		boats.add(new Boat(5));
+		boats.add(new Boat(4));
+		boats.add(new Boat(3));
+		boats.add(new Boat(3));
+		boats.add(new Boat(2));
 	}
 	
-	
-	
-	
-	
+	public void setBoatPosition(int initialRow, int finalRow, int initialCloumn, int finalColumn) {
+		boats.get(currentBoats++).setCoordinates(initialRow, finalRow, initialCloumn, finalColumn);
+	}
+
 }
